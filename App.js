fb9f163ac6f8bbf0fbc18056c03ae20929ce9751
omar saade <omarsaadeh11@gmail.com>
2023-03-30 00:20:14 +0300

    reactnative app

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
-function App() {
+const App = () => {
+  const [enteredGoalText, setEnteredGoalText] = useState();
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    // console.log(courseGoals); //  [{"text":"Hello", key:3333442 }]
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      {text: enteredGoalText, id: Math.random() * 1000},
+    ]);
+  }
   return (
-    <View>
-      <Text>My First App</Text>
+    <View style={styles.appContainer}>
+      <GoalInput
+        onAddGoal={addGoalHandler}
+        onGoalInputHandler={goalInputHandler}
+      />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          keyExtractor={item => item.id}
+          //itemData.item.text
+          renderItem={itemData => <GoalItem text={itemData.item.text} />}
+        />
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1, //wrap content by default
+  },
+  goalsContainer: {
+    flex: 4,
+  },
+});
 
 export default App;
---
+++ components/GoalInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Button, TextInput, StyleSheet} from 'react-native';
+
+const GoalInput = props => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        onChangeText={props.onGoalInputHandler}
+        style={styles.textInput}
+        placeholder="Your course goal!"
+      />
+      <Button title="Add Goal" onPress={props.onAddGoal} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    padding: 8,
+  },
+});
+
+export default GoalInput;
